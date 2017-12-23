81af1758f831ebac40b1a8e71bc8bdef71223911
bharadwaj-raju <bharadwaj.raju@keemail.me>
2017-12-23 22:08:17 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # PySoundGen
-Generate sounds and music from sound waves
+
+Python script to generate sounds and music from sound waves.
+
+Waveform formula from http://code.activestate.com/recipes/578168-sound-generator-using-wav-file/
+
+License: MIT
+
+## Usage
+
+Latest iteration.
+
+Allows you to specify a series of tones, each tone being an ordered triplet `(duration, frequency, amplitude%)`.
+
+    $ python3 soundgen.py {duration},{freq},{amplitude%} [...] [--play]
+
+For example,
+
+    $ python3 soundgen.py 4,44,100 6,440,100 2,440,50 --play
+
+generates a wav audio with 4 seconds of 44 Hz waves at 100% amplitude, then 6s of 440 Hz at 100%, then 2s of 440 Hz at 50%,
+and plays the resulting wav (`--play`). Omit `--play` to only generate audio and not play it.
+
+**NOTE:** The format of a tone must be *exactly* `{duration},{freq},{amplitude%}`. No extra spaces, no order changes, etc.
---
+++ soundgen.py
@@ -0,0 +1,43 @@
+import math
+import sys
+import wave
+import array
+import subprocess as sp
+from collections import namedtuple
+import base64
+import hashlib
+
+# Usage: soundgen.py seconds,freq,amplitude [...] [--play]
+
+Tone = namedtuple('Tone', 'duration freq amplitude')
+
+tones_raw = [x for x in sys.argv[1:] if x != '--play']
+tones = []
+
+for tone_raw in tones_raw:
+	tones.append(Tone(*[int(x) for x in tone_raw.split(',')]))
+
+data = array.array('h') # signed short integer (-32768 to 32767) data
+						# -32768 to 32767 is the max amplitude of a sound card
+
+sample_rate = 44100  # samples/second, 44100 = CD Quality Audio
+n_channels = 1  # of channels (1 = mono, 2 = stereo)
+data_size = 2  # 2 bytes because of using signed short integers => bit depth = 16
+
+for tone in tones:
+	n_samples_per_cycle = int(sample_rate / tone.freq)
+	n_samples = sample_rate * tone.duration
+	for i in range(n_samples):
+		sample = 32767 * float(tone.amplitude) / 100
+		sample *= math.sin(math.pi * 2 * (i % n_samples_per_cycle) / n_samples_per_cycle)
+		data.append(int(sample))
+
+uid = hashlib.md5(base64.b64encode(data.tostring())).hexdigest()
+
+with wave.open('Sound_%s.wav' % uid, 'w') as f:
+	f.setparams((n_channels, data_size, sample_rate, n_samples, "NONE", "Uncompressed"))
+	f.writeframes(data.tostring())
+
+if '--play' in sys.argv:
+	sp.Popen(['aplay', 'Sound_%s.wav' % uid]).wait()
+
